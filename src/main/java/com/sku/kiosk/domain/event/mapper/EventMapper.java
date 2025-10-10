4fafffb1e8c10129add1044a40800be39884d007
Ksieon <keumsiun0503@gmail.com>
2025-10-10 23:07:32 +0900

    :recycle: Refactor: 이벤트 예외처리, Mapper, Repository 리팩토링

--- src/main/java/com/sku/kiosk/domain/event/mapper/EventMapper.java
+++ src/main/java/com/sku/kiosk/domain/event/mapper/EventMapper.java
@@ -1,4 +1,65 @@
+/* 
+ * Copyright (c) SKU K-IO-SK 
+ */
 package com.sku.kiosk.domain.event.mapper;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sku.kiosk.domain.event.dto.response.DetailEventResponse;
+import com.sku.kiosk.domain.event.dto.response.ListEventResponse;
+import com.sku.kiosk.domain.event.dto.response.MainEventResponse;
+import com.sku.kiosk.domain.event.dto.response.WrapperMainEventResponse;
+import com.sku.kiosk.domain.event.entity.Event;
+
+@Component
 public class EventMapper {
+
+  public ListEventResponse toListEventResponse(Event event) {
+    return ListEventResponse.builder()
+        .eventId(event.getId())
+        .title(event.getTitle())
+        .location(event.getLocation())
+        .startDate(event.getStartDate())
+        .endDate(event.getEndDate())
+        .eventCategory(event.getEventCategory())
+        .mainImage(event.getMainImage())
+        .build();
+  }
+
+  public DetailEventResponse toDetailResponse(Event event) {
+    return DetailEventResponse.builder()
+        .eventId(event.getId())
+        .title(event.getTitle())
+        .location(event.getLocation())
+        .startDate(event.getStartDate())
+        .endDate(event.getEndDate())
+        .eventTime(event.getEventTime())
+        .eventCategory(event.getEventCategory())
+        .recruitTarget(event.getRecruitTarget())
+        .mainImage(event.getMainImage())
+        .price(event.getPrice())
+        .address(event.getAddress())
+        .inquiry(event.getInquiry())
+        .latitude(event.getLatitude())
+        .longitude(event.getLongitude())
+        .build();
+  }
+
+  public WrapperMainEventResponse<MainEventResponse> toWrapperMainEventResponse(
+      List<Event> events) {
+    return WrapperMainEventResponse.<MainEventResponse>builder()
+        .eventCategory(events.getFirst().getEventCategory())
+        .events(events.stream().map(this::toMainEventResponse).toList())
+        .build();
+  }
+
+  private MainEventResponse toMainEventResponse(Event event) {
+    return MainEventResponse.builder()
+        .eventId(event.getId())
+        .mainImage(event.getMainImage())
+        .eventCategory(event.getEventCategory())
+        .build();
+  }
 }
--- src/main/java/com/sku/kiosk/domain/event/repository/EventRepository.java
+++ src/main/java/com/sku/kiosk/domain/event/repository/EventRepository.java
@@ -3,33 +3,43 @@
  */
 package com.sku.kiosk.domain.event.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sku.kiosk.domain.event.entity.Event;
+import com.sku.kiosk.domain.event.entity.EventCategory;
+import com.sku.kiosk.domain.event.entity.Status;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-  @Query("SELECT e FROM Event e")
-  Page<Event> findByAll(Pageable pageable);
+  List<Event> findTop3ByEventCategoryOrderByEndDateAsc(EventCategory eventCategory);
 
-  // 제목으로 검색 (부분 일치)
-  List<Event> findByTitleContainingIgnoreCase(String title);
+  Page<Event>
+      findByTitleContainingAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndStatusOrderByEndDateAsc(
+          String keyword,
+          LocalDate endOfWeek,
+          LocalDate startOfWeek,
+          Status status,
+          Pageable pageable);
 
-  // 기간별
-  // Page<Event> findPeriodAllNoCategory(Pageable pageable); // 카테고리 x all
+  Page<Event>
+      findByEventCategoryAndTitleContainingAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndStatusOrderByEndDateAsc(
+          EventCategory eventCategory,
+          String keyword,
+          LocalDate endOfWeek,
+          LocalDate startOfWeek,
+          Status status,
+          Pageable pageable);
 
-  //  Page<Event> findByTodayNoCategory(Pageable pageable);
-  //
-  //  Page<Event> findByThisWeekNoCategory(Pageable pageable);
-  //
-  //  Page<Event> findByThisMonthNoCategory(Pageable pageable);
+  Page<Event> findByTitleContainingAndStatusOrderByEndDateAsc(
+      String keyword, Status status, Pageable pageable);
 
-  //
+  Page<Event> findByEventCategoryAndTitleContainingAndStatusOrderByEndDateAsc(
+      EventCategory eventCategory, String keyword, Status status, Pageable pageable);
 }
