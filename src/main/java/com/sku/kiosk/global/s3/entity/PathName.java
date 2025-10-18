5167c981023f36053e6f3fbf37f4a2f9e9ebc058
Ksieon <keumsiun0503@gmail.com>
2025-10-18 17:49:35 +0900

    :sparkles: Feat: S3 기본 세팅

--- src/main/java/com/sku/kiosk/global/s3/entity/PathName.java
+++ src/main/java/com/sku/kiosk/global/s3/entity/PathName.java
@@ -3,4 +3,9 @@
  */
 package com.sku.kiosk.global.s3.entity;
 
-public enum PathName {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public enum PathName {
+    @Schema(description = "메인 이미지")
+    MAIN,
+}
