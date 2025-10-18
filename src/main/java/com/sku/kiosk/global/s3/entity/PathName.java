5e3f4935539d3da86508fa4a57702efac51cda0c
Ksieon <keumsiun0503@gmail.com>
2025-10-18 17:52:53 +0900

    :recycle: Refactor: CI 적용

--- src/main/java/com/sku/kiosk/global/s3/entity/PathName.java
+++ src/main/java/com/sku/kiosk/global/s3/entity/PathName.java
@@ -6,6 +6,6 @@ package com.sku.kiosk.global.s3.entity;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public enum PathName {
-    @Schema(description = "메인 이미지")
-    MAIN,
+  @Schema(description = "메인 이미지")
+  MAIN,
 }
