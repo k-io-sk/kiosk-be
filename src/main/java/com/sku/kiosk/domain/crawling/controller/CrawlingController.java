762cb4b81fab0a0f3083f5829aa557215b7995a3
Comeoksal <keumsiun0503@gmail.com>
2025-12-09 00:39:28 +0900

    :sparkles: Feat: crawling test api

--- src/main/java/com/sku/kiosk/domain/crawling/controller/CrawlingController.java
+++ src/main/java/com/sku/kiosk/domain/crawling/controller/CrawlingController.java
@@ -1,20 +1,25 @@
+/* 
+ * Copyright (c) SKU K-IO-SK 
+ */
 package com.sku.kiosk.domain.crawling.controller;
 
-import com.sku.kiosk.domain.crawling.service.CrawlingService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sku.kiosk.domain.crawling.service.CrawlingService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/crawling")
 public class CrawlingController {
 
-    private final CrawlingService crawlingService;
+  private final CrawlingService crawlingService;
 
-    @PostMapping("/update")
-    public void updateCrawling() {
-        crawlingService.updateCrawlingEvent();
-    }
+  @PostMapping("/update")
+  public void updateCrawling() {
+    crawlingService.updateCrawlingEvent();
+  }
 }
