5886856d0116edc8371d044b05a0e6cdde065b31
Comeoksal <keumsiun0503@gmail.com>
2025-12-08 22:53:21 +0900

    :sparkles: Feat: activate Scheduler

---
+++ src/main/java/com/sku/kiosk/global/config/SchedulerConfig.java
@@ -0,0 +1,11 @@
+/* 
+ * Copyright (c) SKU K-IO-SK 
+ */
+package com.sku.kiosk.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {}
