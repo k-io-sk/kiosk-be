cf6b920e4a8354c285a1ad1df636c10bc33a57a8
Ksieon <keumsiun0503@gmail.com>
2025-12-10 01:43:21 +0900

    :sparkles: Feat: S3 관련 Config 및 service코드

--- src/main/java/com/sku/kiosk/global/config/S3Config.java
+++ src/main/java/com/sku/kiosk/global/config/S3Config.java
@@ -1,4 +1,60 @@
+/* 
+ * Copyright (c) SKU K-IO-SK 
+ */
 package com.sku.kiosk.global.config;
 
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import lombok.Getter;
+
+@Getter
+@Configuration
 public class S3Config {
+
+  private AWSCredentials awsCredentials;
+
+  @Value("${cloud.aws.credentials.accessKey}")
+  private String accessKey;
+
+  @Value("${cloud.aws.credentials.secretKey}")
+  private String secretKey;
+
+  @Value("${cloud.aws.region.static}")
+  private String region;
+
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
+
+  @Value("${cloud.aws.s3.path.m}")
+  private String mainImagePath;
+
+  @PostConstruct
+  public void init() {
+    this.awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+  }
+
+  @Bean
+  public AmazonS3 s3Client() {
+    AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    return AmazonS3ClientBuilder.standard()
+        .withRegion(region)
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+        .build();
+  }
+
+  @Bean
+  public AWSCredentialsProvider awsCredentialsProvider() {
+    return new AWSStaticCredentialsProvider(awsCredentials);
+  }
 }
--- src/main/java/com/sku/kiosk/global/s3/service/S3ServiceImpl.java
+++ src/main/java/com/sku/kiosk/global/s3/service/S3ServiceImpl.java
@@ -3,4 +3,90 @@
  */
 package com.sku.kiosk.global.s3.service;
 
-public class S3ServiceImpl {}
+import java.io.ByteArrayInputStream;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.sku.kiosk.global.config.S3Config;
+import com.sku.kiosk.global.exception.CustomException;
+import com.sku.kiosk.global.s3.entity.PathName;
+import com.sku.kiosk.global.s3.exception.S3ErrorCode;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class S3ServiceImpl implements S3Service {
+
+  private final S3Config s3Config;
+  private final AmazonS3 amazonS3;
+
+  @Override
+  public String uploadWebp(byte[] webpBytes, String keyName, String fileType) {
+    try {
+      ObjectMetadata metadata = new ObjectMetadata();
+      metadata.setContentLength(webpBytes.length);
+      metadata.setContentType("image/" + fileType);
+
+      ByteArrayInputStream inputStream = new ByteArrayInputStream(webpBytes);
+
+      amazonS3.putObject(
+          new PutObjectRequest(s3Config.getBucket(), keyName, inputStream, metadata));
+
+      return amazonS3.getUrl(s3Config.getBucket(), keyName).toString();
+    } catch (Exception e) {
+      log.error("S3 upload 중 문제 발생", e);
+      throw new CustomException(S3ErrorCode.FILE_SERVER_ERROR);
+    }
+  }
+
+  @Override
+  public String createKeyName(PathName pathName, String fileType) {
+    return getPrefix(pathName) + '/' + UUID.randomUUID() + "." + fileType;
+  }
+
+  @Override
+  public void deleteFile(String keyName) {
+    filedExists(keyName);
+
+    try {
+      amazonS3.deleteObject(new DeleteObjectRequest(s3Config.getBucket(), keyName));
+      log.info("파일 삭제 성공 - keyName: {}", keyName);
+    } catch (Exception e) {
+      log.error("S3 delete 중 오류 발생", e);
+      throw new CustomException(S3ErrorCode.FILE_SERVER_ERROR);
+    }
+  }
+
+  @Override
+  public String extractKetNameFromUrl(String imageUrl) {
+    String bucketUrl =
+        "https://" + s3Config.getBucket() + ".s3." + s3Config.getRegion() + ".amazonaws.com/";
+
+    if (!imageUrl.startsWith(bucketUrl)) {
+      throw new CustomException(S3ErrorCode.FILE_URL_INVALID);
+    }
+    String keyName = imageUrl.substring(bucketUrl.length());
+    log.info("keyName 추출 성공 - keyName: {}", keyName);
+    return keyName;
+  }
+
+  private void filedExists(String keyName) {
+    if (!amazonS3.doesObjectExist(s3Config.getBucket(), keyName)) {
+      throw new CustomException(S3ErrorCode.FILE_NOT_FOUND);
+    }
+  }
+
+  private String getPrefix(PathName pathName) {
+    return switch (pathName) {
+      case MAIN -> s3Config.getMainImagePath();
+    };
+  }
+}
